1219b7b4d8e6b955be1573a49b3e4b2823d90adc
Maica <maicasantos90@gmail.com>
2022-04-29 00:14:56 -0700

    phase 2 complete error handler 2 appears to work

--- authentication/backend/routes/index.js
+++ authentication/backend/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 //route works when req.csrfToken is NOT invoked. why?
 router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken);
+  res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
 
