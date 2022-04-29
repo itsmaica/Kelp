12bf0472cb213da86e012d55585d1b25fcf4b9e2
Maica <maicasantos90@gmail.com>
2022-04-29 11:14:53 -0700

    passed user login testing

--- authentication/backend/routes/api/index.js
+++ authentication/backend/routes/api/index.js
@@ -4,35 +4,50 @@ const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.j
 const { User } = require('../../db/models');
 
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
 
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-      return res.json(req.user);
-    }
-  );
+router.use('/session', sessionRouter);
 
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-      return res.json(req.user);
-    }
-  );
+router.use('/users', usersRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+//passed - setTokenCookie
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+
+//Passed restoreUser
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//       return res.json(req.user);
+//     }
+//   );
+
+//Passed requireAuth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//       return res.json(req.user);
+//     }
+//   );
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
 
 module.exports = router;
---
+++ authentication/backend/routes/api/session.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+//log in
+router.post(
+    '/',
+    asyncHandler(async (req, res, next) => {
+      const { credential, password } = req.body;
+
+      const user = await User.login({ credential, password });
+
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user
+      });
+    })
+  );
+
+module.exports = router;
+
+//testing-logging in with username - PASSED
+// fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `Yfp4cshp-l-D3WmgZKvXoDu945Szo28r_MtI`
+//     },
+//     body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//testing-logging in with email - PASSED
+// fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `Yfp4cshp-l-D3WmgZKvXoDu945Szo28r_MtI`
+//     },
+//     body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//testing credential and password
+// fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `Yfp4cshp-l-D3WmgZKvXoDu945Szo28r_MtI`
+//     },
+//     body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
+//   }).then(res => res.json()).then(data => console.log(data));
