8fbfe758105ccd826140230359c55b943e0aceaf
Maica <maicasantos90@gmail.com>
2022-04-29 13:41:57 -0700

    testing sign up works. error for username arleady exists comes up, but do not yet see email

--- authentication/backend/routes/api/users.js
+++ authentication/backend/routes/api/users.js
@@ -1,4 +1,41 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+//sign up
+router.post(
+    '/',
+    asyncHandler(async (req, res) => {
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user
+      });
+    })
+  );
+
 module.exports = router;
+
+//testing sign-up
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `P3WzpvQ8-PZJD7dgEeUSSMMIHjht0KSnyZzk`
+//     },
+//     body: JSON.stringify({
+//       email: 'spidey@spider.man',
+//       username: 'Spidey',
+//       password: 'password'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//signup error for username already existing works. do not see email tho? 
