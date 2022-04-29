1d27c011ad5ad51aff0395b707660e45f76a88a0
Maica <maicasantos90@gmail.com>
2022-04-29 10:43:35 -0700

    passed phase 3 testing

--- authentication/backend/routes/api/index.js
+++ authentication/backend/routes/api/index.js
@@ -1,10 +1,38 @@
 // backend/routes/api/index.js
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 const router = require('express').Router();
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
+
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
 module.exports = router;
-
-
