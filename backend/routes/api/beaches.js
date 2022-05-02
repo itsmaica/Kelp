5ab4eb50ead89e6c052cc66fa2a8d0d24d54cddf
Maica <maicasantos90@gmail.com>
2022-05-02 13:21:04 -0700

    beaches validation and route

--- backend/routes/api/beaches.js
+++ backend/routes/api/beaches.js
@@ -4,3 +4,19 @@ const asyncHandler = require('express-async-handler')
 //not sure if needed yet
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 
+//need to make Beaches table/model
+const { Beach } = require('../../db/models');
+
+//validations needed?
+//make sure the form is filled out correctly
+
+const router = express.Router();
+
+//create a beach
+// router.post(
+//     "/beaches",
+//     //beach validation needed?
+//     asyncHandler(async(req, res, next) => {
+
+//     })
+// )
